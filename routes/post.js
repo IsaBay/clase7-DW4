788e87386018ebc102f6e1fa7a643f7e39ad3207
IsaBay <isabellabaylac@gmail.com>
2024-11-06 16:33:39 -0300

    primer commit

---
+++ routes/post.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import {getPosts} from '../controllers/get.js';
+import { getPostId } from '../controllers/getid.js';
+import { postPost } from '../controllers/post.js';
+import { updatePost } from '../controllers/update.js';
+import {deletePost} from '../controllers/delete.js'
+
+const router = express.Router();
+
+router.post('/', postPost);
+router.get('/:id', getPostId);
+router.get('/', getPosts)
+
+export const postRoutes = router;
